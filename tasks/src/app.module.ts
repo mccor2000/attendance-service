d466414bdeb141067c600a7a7c191bd0fcba2ca5
Tran Minh Thang <thangmtran@gmail.com>
2023-01-11 04:45:44 +0700

    chore(tasks): cleanup

--- tasks/src/app.module.ts
+++ tasks/src/app.module.ts
@@ -1,12 +1,14 @@
 import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    RedisModule.forRoot({ config: { url: 'redis://localhost:6379'}})
+    RedisModule.forRoot({ config: { url: process.env.REDIS_URL || 'redis://localhost:6379'}}),
+    MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost:27017')
   ],
   providers: [AppService],
 })
--- tasks/src/app.service.ts
+++ tasks/src/app.service.ts
@@ -1,8 +1,10 @@
 import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
 import { Cron } from '@nestjs/schedule';
-import AsyncRetry from 'async-retry';
+import * as AsyncRetry from 'async-retry';
 import Redis from 'ioredis';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
@@ -10,40 +12,46 @@ export class AppService {
 
   constructor(
     @InjectRedis()
-    private readonly redis: Redis
+    private readonly redis: Redis,
+    @InjectConnection()
+    private readonly mongo: Connection
   ) { }
 
-  @Cron('0 0 0 * * *')
-  async flushRedis(): Promise<void> {
-    try {
-      await AsyncRetry(
-        async () => { 
-          await this.redis.flushall()
-        }, {
-        onRetry: (err, attempt) => {
-          this.logger.error(`Error flushing redis, executing retry ${attempt}/3`, err)
-        },
-        retries: 3, 
-      })
-    } catch (err) {
-      this.logger.error('Error flushing redis: ', err)
-    }
-  }
-
+  // 0:0 every day
   @Cron('0 0 0 * * *')
   async processDeadLetterQueue(): Promise<void> {
     try {
       await AsyncRetry(
-        async () => { 
+        async () => {
           // TODO:
         }, {
         onRetry: (err, attempt) => {
           this.logger.error(`Error processing DLQ, executing retry ${attempt}/3`, err)
         },
-        retries: 3, 
+        retries: 3,
       })
     } catch (err) {
       this.logger.error('Error process DLQ: ', err)
+      // further operations. e.g: emailing,..
+    }
+  }
+
+  @Cron('0 0 0 * * *')
+  async flushRedis(): Promise<void> {
+    try {
+      await AsyncRetry(
+        async () => {
+          await this.redis.flushall()
+          this.logger.log('Redis flushed.')
+        }, {
+        onRetry: (err, attempt) => {
+          this.logger.error(`Error flushing redis, executing retry ${attempt}/3`, err)
+        },
+        retries: 3,
+      })
+    } catch (err) {
+      this.logger.error('Error flushing redis: ', err)
+      // further operations. e.g: emailing,..
     }
   }
 }
